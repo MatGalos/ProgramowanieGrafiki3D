9608763366629e7a8aa64f17399bd851ce117064
MatGalos <mateuszgalos99@gmail.com>
2024-12-07 14:06:15 +0100

    Init Uniforms

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -92,6 +92,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/Users/mateuszgalos/uczelnia/Programowanie grafiki 3D zaliczenie/build/src/Assignments/Indices/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/mateuszgalos/uczelnia/Programowanie grafiki 3D zaliczenie/build/src/Assignments/Uniforms/cmake_install.cmake")
+endif()
+
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
---
+++ src/Assignments/Uniforms/shaders/base_fs.glsl
@@ -0,0 +1,8 @@
+#version 420
+
+layout(location=0) out vec4 vFragColor;
+layout(location=1) in vec4 vertex_color;
+
+void main() {
+    vFragColor = vertex_color;
+}
